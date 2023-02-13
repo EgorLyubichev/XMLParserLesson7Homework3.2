3b1a3e167f956a0d3542689d81ef55001523182f
Egor Lyubichev <EgorLyubichev@mail.ru>
2023-02-13 21:20:59 +0300

    Create the first part of tests for XMLParser.java

---
+++ src/test/java/parser/XMLParserTest.java
@@ -0,0 +1,57 @@
+package parser;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class XMLParserTest {
+
+    @Test
+    public void testTrueSimpleXmlFile() {
+        XMLParser parser = new XMLParser("src/test/xmlfiles/true_simple_file.xml");
+        parser.parseDocument();
+        assertTrue(parser.getErrors().isEmpty());
+    }
+
+    @Test
+    public void testSelfClosedTag() {
+        XMLParser parser = new XMLParser("src/test/xmlfiles/file_with_selfclosed_tag.xml");
+        parser.parseDocument();
+        assertTrue(parser.getErrors().isEmpty());
+    }
+
+    @Test
+    public void testTagWithAtribute() {
+        XMLParser parser = new XMLParser("src/test/xmlfiles/tag_with_atribute.xml");
+        parser.parseDocument();
+        assertTrue(parser.getErrors().isEmpty());
+    }
+
+    @Test
+    public void testCommentLines() {
+        XMLParser parser = new XMLParser("src/test/xmlfiles/file_with_comment_lines.xml");
+        parser.parseDocument();
+        assertTrue(parser.getErrors().isEmpty());
+    }
+
+    @Test
+    public void testDifficultFile() {
+        XMLParser parser = new XMLParser("src/test/xmlfiles/difficult_file.xml");
+        parser.parseDocument();
+        assertTrue(parser.getErrors().isEmpty());
+    }
+
+    @Test
+    public void testWithoutDeclarationFile() {
+        XMLParser parser = new XMLParser("src/test/xmlfiles/without_declaration.xml");
+        parser.parseDocument();
+        assertFalse(parser.getErrors().isEmpty());
+    }
+
+
+
+
+
+
+}
